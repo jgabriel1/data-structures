9054d5da08008c9b3675afb50e0cc029c9040c96
jgabriel1 <josegabriel@det.ufc.br>
2020-11-29 20:57:09 -0300

    Created the tree class with insert and search methods.

--- binary_tree/main.py
+++ binary_tree/main.py
@@ -41,3 +41,50 @@ class Node(Generic[T]):
 
     def set_right(self, right: "Node[T]") -> None:
         self._right = right
+
+
+class BinaryTree(Generic[T]):
+
+    _root: Optional[Node[T]]
+
+    def __init__(self) -> None:
+        self._root = None
+
+    def __contains__(self, value: T) -> bool:
+        return self.contains(value)
+
+    def insert(self, value: T) -> None:
+        current = self._root
+
+        if current is None:
+            self._root = Node(value)
+            return
+
+        while True:
+            if value < current.data:
+                if current.get_left() is None:
+                    current.set_left(Node(value))
+                    break
+                else:
+                    current = current.get_left()
+            elif value > current.data:
+                if current.get_right() is None:
+                    current.set_right(Node(value))
+                    break
+                else:
+                    current = current.get_right()
+            else:
+                break
+
+    def contains(self, value: T) -> bool:
+        current = self._root
+
+        while current:
+            if value < current.data:
+                current = current.get_left()
+            elif value > current.data:
+                current = current.get_right()
+            else:
+                return True
+        else:
+            return False
